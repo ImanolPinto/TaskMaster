0bc6951e0d04acda71b9ad551cdb00b4c95dc421
Imanol Pinto <imanolpintolopez@gmail.com>
2014-01-11 11:02:54 +0100

    Improved test readability

--- TaskMaster.Test/PlayedTimesSummaryModelTests.cs
+++ TaskMaster.Test/PlayedTimesSummaryModelTests.cs
@@ -20,11 +20,8 @@ namespace TaskMaster.Test
             Mock<ITimeProvider> timeProviderMock = new Mock<ITimeProvider>();
             var todayDate = new DateTime(1000, 2, 15);
             timeProviderMock.Setup(x => x.Now()).Returns(todayDate);
-
-            // When
             var sut = new PlayedTimesSummaryModel(timeProviderMock.Object);
 
-            // Then
             // When
             Messenger.Default.Send(new OpenPlayedTimesSummaryMsg(null));
 
@@ -36,21 +33,41 @@ namespace TaskMaster.Test
         public void Given_a_time_provider_when_the_played_times_summary_receives_a_played_task_list_then_the_time_items_are_created_only_for_today()
         {
             // Given
-            Mock<ITimeProvider> timeProviderMock = new Mock<ITimeProvider>();
-            var todayDate = new DateTime(1000, 2, 15);
+            var targetDate = new DateTime(1000, 2, 15);
             var otherDate = new DateTime(1000, 2, 14);
-            timeProviderMock.Setup(x => x.Now()).Returns(todayDate);
-            var sut = new PlayedTimesSummaryModel(timeProviderMock.Object);
+            var sut = SutWithTimeProviderReturningATargetDate(targetDate);
+            var taskItemList = TaskItemListWithTasksPlayedTheSameTargetDate(targetDate, otherDate);
+            sut.RegisterForMessagesBeforeView();
 
+            // When
+            Messenger.Default.Send(new OpenPlayedTimesSummaryMsg(taskItemList));
+
+            // Then
+            Assert.IsTrue(OnlyTasksItemsForTheTargetDateAreInTheTimeListItemList(sut.TimeListItems.ToList()));
+        }
+
+
+        #region Helpers
+
+        private static PlayedTimesSummaryModel SutWithTimeProviderReturningATargetDate(DateTime targetDate)
+        {
+            Mock<ITimeProvider> timeProviderMock = new Mock<ITimeProvider>();
+            timeProviderMock.Setup(x => x.Now()).Returns(targetDate);
+            var sut = new PlayedTimesSummaryModel(timeProviderMock.Object);
+            return sut;
+        }
+
+        private static List<TaskItem> TaskItemListWithTasksPlayedTheSameTargetDate(DateTime targetDate, DateTime otherDate)
+        {
             var playSessionList1 = new List<PlaySession>()
             {
-                new PlaySession(todayDate, new TimeSpan(1, 3, 4)),
+                new PlaySession(targetDate, new TimeSpan(1, 3, 4)),
                 new PlaySession(otherDate, new TimeSpan(4, 3, 4))
             };
 
             var playSessionList2 = new List<PlaySession>()
             {
-                new PlaySession(todayDate, new TimeSpan(3, 1, 1))
+                new PlaySession(targetDate, new TimeSpan(3, 1, 1))
             };
 
             var taskItemList = new List<TaskItem>()
@@ -58,16 +75,16 @@ namespace TaskMaster.Test
                 new TaskItemBuilder(Guid.NewGuid()).WithDescription("First task").WithTag("This tag").WithPlaySessions(playSessionList1).Build(),
                 new TaskItemBuilder(Guid.NewGuid()).WithDescription("Other task").WithTag("This tag").WithPlaySessions(playSessionList2).Build()
             };
-
-            sut.RegisterForMessagesBeforeView();
-
-            // When
-            Messenger.Default.Send(new OpenPlayedTimesSummaryMsg(taskItemList));
-
-            // Then
-            Assert.IsTrue(sut.TimeListItems.Count() == 2);
-            Assert.IsTrue(sut.TimeListItems[0].Time == new TimeSpan(1, 3, 4));
-            Assert.IsTrue(sut.TimeListItems[1].Time == new TimeSpan(3, 1, 1));
+            return taskItemList;
         }
+
+        private bool OnlyTasksItemsForTheTargetDateAreInTheTimeListItemList(List<TimeItem> timeItemList)
+        {
+            return timeItemList.Count == 2
+                && timeItemList[0].Time == new TimeSpan(1, 3, 4)
+                && timeItemList[1].Time == new TimeSpan(3, 1, 1);
+        }
+
+        #endregion
     }
 }
